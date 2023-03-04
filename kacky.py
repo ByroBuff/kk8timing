8bd208d2cfd3e46717dcf4bea8752014f2f4fe3d
ByroBuff <73874814+ByroBuff@users.noreply.github.com>
2023-03-04 21:43:53 +0100

    Add files via upload

--- kacky.py
+++ kacky.py
@@ -1,8 +1,6 @@
 import requests
 import os
 
-os.system("cls")
-
 purple = "\033[38;5;141m"
 orange = "\033[38;5;208m"
 reset = "\033[0m"
@@ -33,28 +31,34 @@ def getServerClosestToMap(map):
             closestDistance = distance
             closestServer = server["serverNumber"]
     return (closestServer, closestDistance * 10)
-                                                                         
-print(purple)
-print(" ")
-print(" ")
-print(" ")
-print("          ██╗  ██╗██╗  ██╗ █████╗     ████████╗██╗███╗   ███╗██╗███╗   ██╗ ██████╗       By Byro")
-print("          ██║ ██╔╝██║ ██╔╝██╔══██╗    ╚══██╔══╝██║████╗ ████║██║████╗  ██║██╔════╝       Version 1.0")
-print("          █████╔╝ █████╔╝ ╚█████╔╝       ██║   ██║██╔████╔██║██║██╔██╗ ██║██║  ███╗")
-print("          ██╔═██╗ ██╔═██╗ ██╔══██╗       ██║   ██║██║╚██╔╝██║██║██║╚██╗██║██║   ██║      Built for kackiest Kacky 8")
-print("          ██║  ██╗██║  ██╗╚█████╔╝       ██║   ██║██║ ╚═╝ ██║██║██║ ╚████║╚██████╔╝      Maps: -151 to -200")
-print("          ╚═╝  ╚═╝╚═╝  ╚═╝ ╚════╝        ╚═╝   ╚═╝╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝       Use it well!")
-print(" ")
-print(" ")
+
+while True:   
+    os.system("cls")                                                               
+    print(purple)
+    print(" ")
+    print(" ")
+    print(" ")
+    print("          ██╗  ██╗██╗  ██╗ █████╗     ████████╗██╗███╗   ███╗██╗███╗   ██╗ ██████╗       By Byro")
+    print("          ██║ ██╔╝██║ ██╔╝██╔══██╗    ╚══██╔══╝██║████╗ ████║██║████╗  ██║██╔════╝       Version 1.0")
+    print("          █████╔╝ █████╔╝ ╚█████╔╝       ██║   ██║██╔████╔██║██║██╔██╗ ██║██║  ███╗")
+    print("          ██╔═██╗ ██╔═██╗ ██╔══██╗       ██║   ██║██║╚██╔╝██║██║██║╚██╗██║██║   ██║      Built for kackiest Kacky 8")
+    print("          ██║  ██╗██║  ██╗╚█████╔╝       ██║   ██║██║ ╚═╝ ██║██║██║ ╚████║╚██████╔╝      Maps: -151 to -200")
+    print("          ╚═╝  ╚═╝╚═╝  ╚═╝ ╚════╝        ╚═╝   ╚═╝╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝       Use it well!")
+    print(" ")
+    print(" ")
 
 
-map = input("          What map (-151 to -200): ")
-if int(map) >= -200 and int(map) <= -150:
-    result = getServerClosestToMap(int(map))
+    map = input("          What map (-151 to -200): ")
+    if int(map) >= -200 and int(map) <= -150:
+        result = getServerClosestToMap(int(map))
+        print(" ")
+        print(f"          Server {orange + str(result[0]) + purple} will play {orange + '#' + str(map) + purple} in abount {orange + str(result[1]) + purple} minutes.")
+    else:
+        print(" ")
+        print("Invalid map number, must be between -200 and -150.")
+
+
     print(" ")
-    print(f"          Server {orange + str(result[0]) + purple} will play {orange + '#' + str(map) + purple} in abount {orange + str(result[1]) + purple} minutes.")
-else:
     print(" ")
-    print("Invalid map number, must be between -200 and -150.")
-    
-input("Press enter to exit...")
+    print(" ")
+    input("Press enter to restart...")
