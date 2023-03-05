9a731a9197ca0a841ac19779b26b30bc5206e450
ByroBuff <73874814+ByroBuff@users.noreply.github.com>
2023-03-05 09:12:29 +0100

    Add files via upload

--- kacky.py
+++ kacky.py
@@ -5,32 +5,8 @@ purple = "\033[38;5;141m"
 orange = "\033[38;5;208m"
 reset = "\033[0m"
 
-DASHBOARDURL = "https://api.kacky.info/dashboard"
-dashboard = requests.get(DASHBOARDURL).json()["servers"]
-allMaps = list(reversed(range(-200, -150)))
-
-def reformatList(allMaps, currentMap):
-    return allMaps[allMaps.index(currentMap):] + allMaps[:allMaps.index(currentMap)]
-
-def findNearestMap(currentMap, targetMap):
-    mapList = reformatList(allMaps, currentMap)
-    count = 0
-
-    for map in mapList:
-        if map == targetMap:
-            return count
-        else:
-            count += 1
-
-def getServerClosestToMap(map):
-    closestServer = 0
-    closestDistance = 999
-    for server in dashboard:
-        distance = findNearestMap(server["maps"][0]["number"], map)
-        if distance < closestDistance:
-            closestDistance = distance
-            closestServer = server["serverNumber"]
-    return (closestServer, closestDistance * 10)
+DASHBOARDURL = "https://api.kacky.info/spreadsheet"
+dashboard = requests.get(DASHBOARDURL).json()
 
 while True:   
     os.system("cls")                                                               
@@ -49,16 +25,15 @@ while True:
 
 
     map = input("          What map (-151 to -200): ")
-    if int(map) >= -200 and int(map) <= -150:
-        result = getServerClosestToMap(int(map))
-        print(" ")
-        print(f"          Server {orange + str(result[0]) + purple} will play {orange + '#' + str(map) + purple} in abount {orange + str(result[1]) + purple} minutes.")
+    for server in dashboard:
+        if server["kacky_id"] == map:
+            print(" ")
+            print(f"          Server {orange + str(server['server']) + purple} will play {orange + str(server['kacky_id']) + purple} in abount {orange + str(server['upcomingIn']) + purple} minutes.")
+            break
     else:
-        print(" ")
-        print("Invalid map number, must be between -200 and -150.")
-
+        print("          Invalid map number, must be between -200 and -150.")
 
     print(" ")
     print(" ")
     print(" ")
-    input("Press enter to restart...")
+    input("          Press enter to restart...")
